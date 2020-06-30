e9ca40a04528c7c6505cffcdbe7ca7571837a5e9
cczyWyc <274230928@qq.com>
2020-07-01 01:04:54 +0800

    搭建hystrix仪表盘监控服务

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/shterm/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package com.shterm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * 主启动类
+ * @author wangyc
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
