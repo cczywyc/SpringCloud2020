a2d13f326cd7a66424d07b61fe88075cfe27b3c6
cczyWyc <274230928@qq.com>
2020-07-09 00:41:00 +0800

    配置springcloud微服务配置中心

---
+++ cloud-config-center-3344/src/main/java/com/shterm/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.shterm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 主启动类
+ * @author wangyc
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
