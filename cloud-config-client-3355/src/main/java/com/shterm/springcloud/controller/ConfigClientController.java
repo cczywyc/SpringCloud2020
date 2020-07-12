19341257cf4bfcc4e7a87aedea7e4b626e99f1b5
cczyWyc <274230928@qq.com>
2020-07-12 23:20:07 +0800

    微服务配置中心客户端配置

---
+++ cloud-config-client-3355/src/main/java/com/shterm/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.shterm.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 控制类
+ * @author wangyc
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
